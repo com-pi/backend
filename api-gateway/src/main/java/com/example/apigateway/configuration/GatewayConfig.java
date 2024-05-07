adfa0b127aceee5222cbabf646e4d48861889de7
kihong_kim <berserkppp1@gmail.com>
2024-05-08 00:36:26 +0900

    [feat] cors 설정 추가

---
+++ api-gateway/src/main/java/com/example/apigateway/configuration/GatewayConfig.java
@@ -0,0 +1,28 @@
+package com.example.apigateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://192.168.56.20:8000");
+        config.addAllowedOrigin("http://api.com-p.site:8000");
+        config.addAllowedOrigin("https://api.com-p.site:8000");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
