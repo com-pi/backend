defeb37090c203135cedc9a1340dd70862234569
kihong_kim <berserkppp1@gmail.com>
2024-05-11 02:34:26 +0900

    [fix] 문구 수정, 설정 수정

--- auth-service/src/main/java/com/example/authserver/adapter/in/TokenController.java
+++ auth-service/src/main/java/com/example/authserver/adapter/in/TokenController.java
@@ -15,8 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/")
 @Tag(name = "토큰 재발급", description = "리프레시 토큰을 이용하여 토큰을 재발급 합니다. <br>" +
-        "토큰 검증 단계에서 엑세스 토큰이 없거나 만료 되었는데 리프레시 토큰을 가지고 있으면 자동으로 재발급 시도를 합니다. <br>" +
-        "리프레시 토큰은 쿠키(HttpOnly)로 전달 됩니다.")
+        "토큰 검증 단계에서 엑세스 토큰이 없거나 만료 되었는데 리프레시 토큰을 가지고 있으면 자동으로 재발급 시도를 합니다.")
 public class TokenController {
 
     private final TokenReissueService tokenReissueService;
