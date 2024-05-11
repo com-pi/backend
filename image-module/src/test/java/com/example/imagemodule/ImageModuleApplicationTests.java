d488393c4c496a3b82990fc793164e5427c7fbd3
kihong_kim <berserkppp1@gmail.com>
2024-05-11 11:13:02 +0900

    [fix] 불필요한 파일 삭제

---
+++ image-module/src/test/java/com/example/imagemodule/ImageModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imagemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
