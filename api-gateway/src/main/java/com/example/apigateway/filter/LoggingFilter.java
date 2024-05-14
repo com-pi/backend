703a4321d9c59d62368dedb26a0e46fc9f1ac742
kihongkim <me@d.com>
2024-05-14 12:11:30 +0900

    [fix] CORS Allow Origin Header 수정

--- api-gateway/src/main/java/com/example/apigateway/filter/LoggingFilter.java
+++ api-gateway/src/main/java/com/example/apigateway/filter/LoggingFilter.java
@@ -4,11 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.Objects;
 
 @Slf4j
 @Component
@@ -22,13 +31,28 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
     public GatewayFilter apply(Config config) {
         return new OrderedGatewayFilter(
                 (exchange, chain) -> {
-                    // Todo: 요청 로깅
+                    logRequest(exchange);
+
                     return chain.filter(exchange)
-                            .then(Mono.fromRunnable(() -> {
-                                // Todo: 응답로깅
-                            }
-                            ));},
-                -100);
+                            .then(Mono.fromRunnable(() -> logResponse(exchange)));
+                }, -100);
+    }
+
+    private void logRequest(ServerWebExchange exchange) {
+        HttpHeaders headers = exchange.getRequest().getHeaders();
+        HttpMethod method = exchange.getRequest().getMethod();
+        URI uri = exchange.getRequest().getURI();
+
+        Logger logger = LoggerFactory.getLogger(GatewayFilter.class);
+        logger.info("Request {} {} Headers: {}", method, uri, headers);
+    }
+
+    private void logResponse(ServerWebExchange exchange) {
+        HttpHeaders headers = exchange.getResponse().getHeaders();
+        HttpStatusCode statusCode = exchange.getResponse().getStatusCode();
+
+        Logger logger = LoggerFactory.getLogger(GatewayFilter.class);
+        logger.info("Response status {} Headers: {}", Objects.requireNonNull(statusCode).value(), headers);
     }
 
 
