115506d4ec3b1fec09ca86cb676ccd6e6c1ff524
sbhan <hsb9710@naver.com>
2024-05-20 05:05:15 +0900

    [feat] my-plant 내 이미지 포트 관련 파일 imageModule 사용으로 제거

--- my-plant/src/main/java/com/example/myplant/application/port/out/ImageUploadPort.java
+++
@@ -1,9 +0,0 @@
-package com.example.myplant.application.port.out;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
-
-public interface ImageUploadPort {
-    List<String> uploadImages(List<MultipartFile> images);
-}
