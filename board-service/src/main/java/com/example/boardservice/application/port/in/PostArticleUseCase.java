df1e6e0ecdfc5e4efd8e1762ce78d021fdff765d
nagool <quietwave00@gmail.com>
2024-06-03 02:18:30 +0900

    [refactor] 클래스 네임 변경

--- board-service/src/main/java/com/example/boardservice/application/port/in/PostArticleUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.example.boardservice.application.port.in;
-
-import com.example.boardservice.adapter.in.web.command.PostBuyAndSellCommand;
-
-public interface PostArticleUseCase {
-
-    Long postBuyAndSell(PostBuyAndSellCommand command);
-
-}
