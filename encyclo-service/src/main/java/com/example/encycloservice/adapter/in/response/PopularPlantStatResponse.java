941228a6cead4426a921d9d3593760ee300e6034
kihongkim <me@d.com>
2024-06-24 14:00:19 +0900

    [fix] 인기식물 조회 기능 디버깅

--- encyclo-service/src/main/java/com/example/encycloservice/adapter/in/response/PopularPlantStatResponse.java
+++ encyclo-service/src/main/java/com/example/encycloservice/adapter/in/response/PopularPlantStatResponse.java
@@ -1,18 +1,41 @@
 package com.example.encycloservice.adapter.in.response;
 
+import com.example.encycloservice.application.port.out.EncyclopediaQuery;
+import com.example.encycloservice.application.port.out.PopularPlantStat;
+import com.example.encycloservice.domain.PlantSpecies;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
 public record PopularPlantStatResponse (
+        String referenceTime,
         List<PlantRankRecord> plantRecord
 ) {
    @Builder
    public record PlantRankRecord (
-           Long id,
+           String id,
+           int rank,
            String commonName,
            String imageUrl
    ) {
+   }
+
+   public static PopularPlantStatResponse of (PopularPlantStat domain, EncyclopediaQuery encyclopediaQuery){
+      List<PlantRankRecord> plantRankRecord = domain.plantRankList().stream().map(
+              plantRank -> {
+                 PlantSpecies plantSpecies = encyclopediaQuery.getById(Long.valueOf(plantRank.plantId()));
+                 return PlantRankRecord.builder()
+                         .id(plantRank.plantId())
+                         .rank(plantRank.rank())
+                         .commonName(plantSpecies.getCommonName())
+                         .imageUrl(plantSpecies.getImageUrls().get(0))
+                         .build();
+              }
+      ).toList();
+      return PopularPlantStatResponse.builder()
+              .referenceTime(domain.referenceTime())
+              .plantRecord(plantRankRecord)
+              .build();
    }
 }
