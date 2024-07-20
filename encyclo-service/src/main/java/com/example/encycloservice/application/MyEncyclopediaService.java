a9a62dc88c12032b2a1de5c8b6204e0aa8160092
kihong_kim <berserkppp1@gmail.com>
2024-07-20 14:24:31 +0900

    [fix] 비동기 요청 수정

--- encyclo-service/src/main/java/com/example/encycloservice/application/MyEncyclopediaService.java
+++ encyclo-service/src/main/java/com/example/encycloservice/application/MyEncyclopediaService.java
@@ -1,6 +1,5 @@
 package com.example.encycloservice.application;
 
-import com.example.common.exception.CommonException;
 import com.example.common.exception.NotFoundException;
 import com.example.encycloservice.aop.filter.PassportHolder;
 import com.example.encycloservice.application.port.in.MyEncyclopediaUseCase;
@@ -16,8 +15,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 @Service
 @RequiredArgsConstructor
@@ -51,48 +48,34 @@ public class MyEncyclopediaService implements MyEncyclopediaUseCase {
                 .orElseThrow(() -> new NotFoundException("내 도감을 찾을 수 없습니다."));
         myEncyclopedia.verifyOwner(PassportHolder.getPassport());
 
-
-        List<CompletableFuture<Void>> futures = plantSpeciesIds.stream()
-                .map(plantSpeciesId -> CompletableFuture.runAsync(() -> {
-                    PlantSpecies plantSpecies = encyclopediaQuery.getById(plantSpeciesId);
-                    if (plantSpecies == null) {
-                        throw new NotFoundException("식물을 찾을 수 없습니다.");
-                    }
-                    try {
-                        myEncyclopediaCommand.addPlantsToEncyclopedia(plantSpecies, myEncyclopedia);
-                    } catch (DataIntegrityViolationException ignored) {
-                        // 내 도감에 동일한 식물이 들어가 있을 때, 이를 무시합니다.
-                    }
-                })).toList();
-
-        CompletableFuture<Void> all = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
-        try {
-            all.get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new CommonException("식물을 찾을 수 없습니다.", 404, e);
-        }
+        plantSpeciesIds.forEach(plantSpeciesId -> {
+            PlantSpecies plantSpecies = encyclopediaQuery.getById(plantSpeciesId);
+            if (plantSpecies == null) {
+                throw new NotFoundException("식물을 찾을 수 없습니다.");
+            }
+            try {
+                myEncyclopediaCommand.addPlantsToEncyclopedia(plantSpecies, myEncyclopedia);
+            } catch (DataIntegrityViolationException ignored) {
+                // 내 도감에 동일한 식물이 들어가 있을 때, 이를 무시합니다.
+            }
+        });
     }
 
     @Override
+    @Transactional
     public void removePlantsFromEncyclopedia(List<Long> plantSpeciesIds, Long myEncyclopediaId) {
         MyEncyclopedia myEncyclopedia = myEncyclopediaQuery.findById(myEncyclopediaId)
                 .orElseThrow(() -> new NotFoundException("내 도감을 찾을 수 없습니다."));
         myEncyclopedia.verifyOwner(PassportHolder.getPassport());
 
-        List<CompletableFuture<Void>> futures = plantSpeciesIds.stream()
-                .map(plantSpeciesId -> CompletableFuture.runAsync(() -> {
+        plantSpeciesIds
+                .forEach(plantSpeciesId -> {
                     PlantSpecies plantSpecies = encyclopediaQuery.getById(plantSpeciesId);
                     if (plantSpecies == null) {
                         throw new NotFoundException("식물을 찾을 수 없습니다.");
                     }
                     myEncyclopediaCommand.removePlantFromEncyclopedia(plantSpecies, myEncyclopedia);
-                })).toList();
-        CompletableFuture<Void> all = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
-        try {
-            all.get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new CommonException("식물을 찾을 수 없습니다.", 404, e);
-        }
+                });
     }
 
     @Override
