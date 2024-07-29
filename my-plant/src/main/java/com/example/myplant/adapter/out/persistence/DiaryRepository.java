c80f2bf4119a08b1f82da2415c90dfe9f7bd8b64
nagool <quietwave00@gmail.com>
2024-07-30 00:04:27 +0900

    [fix] dev 테스트 네이티브 쿼리로 수정

--- my-plant/src/main/java/com/example/myplant/adapter/out/persistence/DiaryRepository.java
+++ my-plant/src/main/java/com/example/myplant/adapter/out/persistence/DiaryRepository.java
@@ -3,6 +3,8 @@ package com.example.myplant.adapter.out.persistence;
 import com.example.myplant.adapter.out.persistence.entity.DiaryEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,7 +12,22 @@ import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<DiaryEntity, Long> {
 
-    List<DiaryEntity> findByCreatedDateBetweenAndMemberIdAndMyPlantIdAndDeletionYn(LocalDate startDate, LocalDate endDate, Long myPlantId, Long memberId, String deletionYn);
+    @Query("""
+        SELECT d
+        FROM DiaryEntity d
+        WHERE d.createdDate >= :startDate
+          AND d.createdDate <= :endDate
+          AND d.memberId = :memberId
+          AND d.myPlantId = :myPlantId
+          AND d.deletionYn = :deletionYn
+    """)
+    List<DiaryEntity> findByCreatedDateBetweenAndMemberIdAndMyPlantIdAndDeletionYn(
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("memberId") Long memberId,
+            @Param("myPlantId") Long myPlantId,
+            @Param("deletionYn") String deletionYn
+    );
 
     List<DiaryEntity> findByMemberIdAndIsPublicAndIsPublishedAndDeletionYn(Long memberId, boolean isPublic, boolean isPublished, String deletionYn, Pageable pageable);
 
