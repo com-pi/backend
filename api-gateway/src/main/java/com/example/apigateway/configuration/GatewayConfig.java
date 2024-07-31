1cecf5ff0099bc4235681f548bea12b017649e25
kihongkim <me@d.com>
2024-07-31 15:55:58 +0900

    [feat] 쿠키 전송 관련

--- api-gateway/src/main/java/com/example/apigateway/configuration/GatewayConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/configuration/GatewayConfig.java
@@ -13,6 +13,7 @@ public class GatewayConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
