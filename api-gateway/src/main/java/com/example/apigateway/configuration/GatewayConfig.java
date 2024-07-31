12d189e3332c840bdff3416e15332efcd6316db3
kihongkim <me@d.com>
2024-07-31 16:15:42 +0900

    [fix] 쿠키 전송 관련 Origin 추가

--- api-gateway/src/main/java/com/example/apigateway/configuration/GatewayConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/configuration/GatewayConfig.java
@@ -15,6 +15,7 @@ public class GatewayConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:5500");
         config.addAllowedOrigin("https://frontend-roan-five-28.vercel.app");
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
