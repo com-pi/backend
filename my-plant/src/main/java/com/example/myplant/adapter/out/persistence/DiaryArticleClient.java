9436f4018ab1e7bbbb32313ba23d428ee4ab77bf
nagool <quietwave00@gmail.com>
2024-08-07 16:25:14 +0900

    [fix] dev feignclient 테스트

--- my-plant/src/main/java/com/example/myplant/adapter/out/persistence/DiaryArticleClient.java
+++ my-plant/src/main/java/com/example/myplant/adapter/out/persistence/DiaryArticleClient.java
@@ -5,9 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "diary-article", url = "${feign.client.config.diary-article.url}")
+@FeignClient(name = "board-service", url = "${feign.client.config.diary-article.url}")
 public interface DiaryArticleClient {
-    @PostMapping
+    @PostMapping("/diaries")
     void postDiaryArticle(
             @RequestBody DiaryArticleRequest request
     );
