25d2ca7f6284ced8fb461b1521b500fcfaf68dc1
kihongkim <me@d.com>
2024-08-29 15:03:18 +0900

    [feat] comppi.site 도메인 cors 추가

--- api-gateway/src/main/java/com/example/apigateway/configuration/GatewayConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/configuration/GatewayConfig.java
@@ -16,7 +16,7 @@ public class GatewayConfig {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOrigin("http://localhost:5500");
-        config.addAllowedOrigin("https://comppi.vercel.app/");
+        config.addAllowedOrigin("https://comppi.site");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
